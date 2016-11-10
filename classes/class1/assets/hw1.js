60b1e816787d4b879f205d4792b0c0a0e77e9234
kerenagrichwd2017 <kerensagrich@gmail.com>
2016-11-10 12:51:29 +0200

    ad

---
+++ classes/class1/assets/hw1.js
@@ -0,0 +1,15 @@
+document.getElementById("myInfo").onclick=changeColor;
+var currentColor="blue";
+
+function changeFontSize(){
+
+    if(currentColor=="blue"){
+        document.button.style.color="red";
+        currentColor="red";
+    }else{
+        document.button.style.color="blue";
+        currentColor="blue";
+    }
+    return currentColor;
+    }
+}
