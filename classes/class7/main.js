cd6f81c53762c6ac9d93a470b7aa52f001d6bf57
kerenagrichwd2017 <kerensagrich@gmail.com>
2016-12-15 12:35:56 +0200

    class 7

---
+++ classes/class7/main.js
@@ -0,0 +1,38 @@
+/*click the button
+    the menu will slide in from the right -> change right to 20px
+    check if its on the page, move it off
+    if its off, move it on*/
+
+$(function(){
+
+    var invisible=true;
+    var myRight="20px";
+
+    $("#clickme").on("click", function(){
+        $("#menu").animate(
+            {right: myRight},
+            {queue:false, duration:500, easing: "linear"}
+        );
+
+        if(invisible){
+            myRight="-100px";
+            invisible=false;
+        }else{
+            myRight="20px";
+            invisible=true;
+        }
+    });
+
+    $("#myform").submit{function(e){   /* e= event*/
+        e.preventDefault();
+    });
+
+    $("#color_red").prop("checked", true);
+    $("input[name='myage']").val("18"); /*sets value for this number*/
+    $("input [type='checkbox']").change(function(){
+       // alert("the value is: " + $(this).val()); /* .val(empty) gives back value*/
+        $(this).next().css("color",$(this).val());
+
+
+    });
+});
