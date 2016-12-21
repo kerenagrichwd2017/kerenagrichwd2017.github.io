7e9824980b650a32409563b21bfed7da906a0758
Keren Agrich <Keren Agrich>
2016-12-21 03:58:38 -0500

    assignment7

--- assignments/class7/circle.js
+++ assignments/class7/circle.js
@@ -1,1 +1,23 @@
-$("myCircle").data("color","hsl(0,"+s+""%"+l+"%")");
+$(function(){
+
+    var h="0";
+    var s="50";
+    var l="50";
+    var color="hsl(0,50%, 50%)";
+
+    $("#circle").on("change mousemove", function(){
+        $("#saturation").animate(
+        {background: }
+         //$("circle").data("color","hsl(0,"+s+""%"+l+"%")");
+        );
+    }
+
+
+
+
+
+}
+
+
+
+
