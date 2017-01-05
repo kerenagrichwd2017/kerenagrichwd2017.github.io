2f1bb64964775773e5177813c4c3972c9bb3e536
kerenagrichwd2017 <kerensagrich@gmail.com>
2017-01-05 13:16:56 +0200

    hw8

--- assignments/class8/main.js
+++ assignments/class8/main.js
@@ -6,25 +6,21 @@ $(function(){
 
     $myNavItem.on("mouseover", function(){
         $(this).find("div").animate(
-        {height:"200px"},
-        {queue:false, duration: 500, easing:"linear",
-         start: function(){
-             $(this).css("");
-         }
-        }
-    );
-}).on("mouseleave", function(){
-    $(this).find("div").animate(
-        {height:"0px"},
-        {queue:false, duration: 500, easing: "linear",
-         complete: function(){
-             $(this).css("");
-         }
-        }
-    );
+            {height:"200px"},
+            {queue:false, duration: 500, easing:"linear"
+
+            }
+        );
+    }).on("mouseleave", function(){
+        $(this).find("div").animate(
+            {height:"0px"},
+            {queue:false, duration: 500, easing: "linear"
+
+            }
+        );
 
     });
-}):
+});
 
 
 
