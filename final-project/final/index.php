8d66f625bc8ba40ec4f971c6e8c35cdd179834e5
Keren Agrich <Keren Agrich>
2017-04-04 07:10:05 -0400

    final project pages

---
+++ final-project/final/index.php
@@ -0,0 +1,339 @@
+<? include("header.html"); ?>
+            <section id="banner">
+                <header>
+                    <h2>Community Garage Inc.</h2>
+                    <a href="#" class="shop"> <img src="shop.PNG"/></a>
+
+                </header>
+            </section>
+            <section class="wrapper style1">
+                <div class="container">
+                    <div class="row 200%">
+                        <section class="4u 12u (narrower)">
+                            <div class="box highlight">
+                                <i class="icon map-location"></i>
+                                <h3>Find Us</h3>
+                                <p>we are locted in.....</p>
+                            </div>
+                        </section>
+                        <section class="4u 12u (narrower)">
+                            <div class="box highlight">
+                                <i class="icon tool-box"></i>
+                                <h3>Services</h3>
+                                <p>we offer a wide range of services.....</p>
+                            </div>
+                        </section>
+                        <section class="4u 12u (narrower)">
+                            <div class="box highlight">
+                                <i class="icon note-pad"></i>
+                                <h3>Reviews</h3>
+                                <p>what our customers have to say about us.....</p>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </section>
+            <section class="wrapper style2">
+                <div class="container">
+                    <header class="major">
+                        <h2>All about Community Garage</h2>
+                        <p>learn about the company</p>
+                    </header>
+                </div>
+            </section>
+            <section class="wrapper style1">
+                <div class="container">
+                    <div class="row">
+                        <section class="6u 12u (narrower)">
+                            <div class="box post">
+                                <a href="#" class="image left"><img src="family-owned.png" alt=" "/></a>
+                                <div class="inner">
+                                    <h3>How the company started</h3>
+                                    <p>family owned company</p>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section class="6u 12u (narrower)">
+                            <div class="box post">
+                                <a href="#" class="image left"><img src="info-icon.png" alt=" "/></a>
+                                <div class="inner">
+                                    <h3>About the owner</h3>
+                                    <p>Avi started the business on his own.....</p>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="row">
+                        <section class="6u 12u (narrower)">
+                            <div class="box post">
+                                <a href="#" class="image left"><img src="lang.jpg" alt=" "/></a>
+                                <div class="inner">
+                                    <h3>Languages</h3>
+                                    <p>English <br> Hebrew <br> Spanish <br> French</p>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="6u 12u (narrower)">
+                            <div class="box post">
+                                <a href="#" class="image left"><img src="map.png" alt =" "></a>
+                                <div class="inner">
+                                    <h3>Google Map Widget</h3>
+                                    <p></p>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </section>
+
+            <section id="cta" class="wrapper style3">
+                <form class="form">
+                    <div class="form-group">
+                        <label for="jquery-search-sample">Search</label>
+                        <input type="text" class="form-control" id="jquery-search-sample">
+                    </div>
+                </form>
+                <table class="table table-bordered table-inverse">
+                    <thead>
+                        <tr>
+<!--                            <th>Car</th>-->
+                            <th>Service</th>
+                            <th>Labor</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="jsearch-row" style="display: table-row;">
+<!--                            <td class="jsearch-field">Acura</td>-->
+                            <td class="jsearch-field">Battery</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+<!--                            <td class="jsearch-field">Audi</td>-->
+                            <td class="jsearch-field">Belts</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Body Repair</td>
+                            <td>$44 per hour</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+<!--                            <td class="jsearch-field">Audi</td>-->
+                            <td class="jsearch-field">Brakes</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Clutch</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Diagnostics</td>
+                            <td>$90.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Doors</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Engine</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Exhaust System</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Filters</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Fluids</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Fuel System</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Heating and AC</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Hoses</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Ignition</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+<!--
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Lights</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+-->
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Mirrors</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Paint</td>
+                            <td>$44 per hour</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Sensors</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Suspension</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Steering</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Switches</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Tires</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Windows</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                        <tr class="jsearch-row" style="display: table-row;">
+                            <td class="jsearch-field">Wiper System</td>
+                            <td>30 minutes</td>
+                            <td>$45.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+
+
+
+
+ <? include("footer.html"); ?>
+
+
+
+
+
+
+<!--
+                <div class="container">
+                    <header>
+                        <h2>Search for your service or car problem here</h2>
+                        <a href="#" class="button"> Search</a>
+                    </header>
+                    <input type="text" id="jquery-search-sample">
+                        <tr class="jsearch-row">
+                            <td class="jsearch-field">Battery</td>
+                            <td class="jsearch-field">Battery Replacement</td>
+                            <td>Service Battery Cable</td>
+                            <td>61</td>
+                            <td>2011/04/25</td>
+                            <td>$320,800</td>
+                        </tr>
+                </div>
+            </section>
+        </div>
+
+
+
+
+
+
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Website Home Page </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="main.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <a href="details.html" id="logo">COMMUNITYGARAGE</a>
+            </h1>
+            <nav id="navigation">
+                <ul>
+                    <li class="current" style="white-space: nowrap;">
+                    <a href="details.html">HOME</a>
+                    </li>
+                    <li class="opener" style="user-select: none; cursor: pointer; white-space: nowrap; opacity:1;"><a href="#about">ABOUT</a>
+                    </li>
+                    <li style="white-space: nowrap;"><a href="#services">SERVICES</a></li>
+                    <li style="white-space: nowrap;"><a  href="prices">PRICES</a></li>
+                </ul>
+
+            </nav>
+        </header>
+    <section id="home">
+        <h2>HOME</h2>
+        <div class="illustration"></div>
+        <div class="container">
+            <iframe> </iframe>
+            <div class="row 200%">
+                <section class="4u 12u(narrower)">
+                    <div class="box highlight">
+                        <i class="icon major fa-paper-plane"></i>
+                        <h3>FIND US</h3>
+                        <p>
+                        Address:
+                        </p>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </section>
+    <section id="clients">
+        <h2>Clients</h2>
+    </section>
+    <section id="about">
+        <h2>About</h2>
+     </section>
+     <section id="contact-us">
+        <h2>Contact Us</h2>
+     </section>
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="main.js"></script>
+
+</body>
+</html>
+-->
